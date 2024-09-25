1621fd20237dab9063d417dbf80fa6901f7bf403
Marcio Curvello <marciocurvello@gmail.com>
2024-09-25 00:57:38 -0300

    student, teacher and parent pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'pt'],  // Define os idiomas suportados
+    defaultLocale: 'pt',    // Define o idioma padrão como português
+  },
+};
 
 export default nextConfig;
