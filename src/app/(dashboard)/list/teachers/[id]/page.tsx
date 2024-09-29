9b01a4d125174cf51eb170635101fe37db72ba18
Marcio Curvello <marciocurvello@gmail.com>
2024-09-29 12:18:50 -0300

    teacher form

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Announcements from "@/components/Announcements";
 import BigCalendar from "@/components/BigCalendar";
+import FormModal from "@/components/FormModal";
 import Performance from "@/components/Performance";
 import Image from "next/image";
 import Link from "next/link";
@@ -23,7 +24,22 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
+            <div className="flex items-center gap-4">
               <h1 className="text-xl font-semibold">Leonard Snyder</h1>
+              <FormModal table="teacher" type="update" data={{
+                id: 1,
+                username: "deanguerrero",
+                password: "password",
+                firstName: "Dean",
+                lastName: "Guerrero",
+                phone: "+1 234 567 89",
+                address: "1234 Main St, Anytown, USA",
+                bloodType: "A+",
+                dateOfBirth: "2000-01-01",
+                sex: "male",
+                img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinyrgb&w=1200"
+              }} />
+              </div>
               <p className="text-sm text-gray-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing.
               </p>
@@ -118,11 +134,21 @@ const SingleTeacherPage = () => {
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-sm text-gray-500">
-            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Teacher&apos;s Classes</Link>
-            <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">Teacher&apos;s Students</Link>
-            <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">Teacher&apos;s Lessons</Link>
-            <Link className="p-3 rounded-md bg-pink-50" href="/">Teacher&apos;s Exams</Link>
-            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Teacher&apos;s Assignments</Link>
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Teacher&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Teacher&apos;s Assignments
+            </Link>
           </div>
         </div>
         <Performance />
